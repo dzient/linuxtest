9b4c7f0baf5b56de4dfbc48bb766c5dafc2639f4
David Zientara <dzient@peoplepc.com>
2020-12-06 13:08:46 -0500

    Committed name.cpp

---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char name[1024];                
+printf("Enter your name: ");
+scanf("%s",name);
+printf("Hello, %s\n",name);
+  
+return 0; //Success
+}
