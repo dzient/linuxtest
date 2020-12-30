c8bc4febe33b9a772382852b8be5ee5103b5b214
David Zientara <dzient@peoplepc.com>
2020-12-29 22:00:16 -0500

    namecplus.cpp added

---
+++ namecplus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int calculateSquareFootage(int length, int width) {
+	return length*width;
+}
+
+int main()
+{
+	char name[1024];
+        cout<<"Enter name: ";
+        cin>>name;
+	cout<<"Hello, "<<name<<endl; 		
+	return 0;
+}
+
