4cdab7eb87dd56f893ba86f73f1c7497c43472e1
David Zientara <dzient@peoplepc.com>
2021-01-10 13:13:32 -0500

    Adding function.cpp

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This is a program with a function."<<endl;
+	func();
+	cout<<"Back in main program."<<endl;
+}
+void func()
+{
+	cout<<"In the function now."<<endl;
+}
+
